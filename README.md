802a59e7159d09314aef3b63985862fecfa252b0
Sunjang1009 <sunjang1009@gmail.com>
2023-02-25 14:47:31 -0500

    updated easy card emojis

---
+++ README.md
@@ -0,0 +1,15 @@
+welcome to my game!
+
+two options!
+# easy : colorful cards
+# hard : black and white cards
+//should I do theme??
+
+game rule
+# press easy or hard button, then start game
+## random image shuffle , click matched and +1 score, if matched relain opened.
+
+bonus features
+### hint
+### time remain
+
--- game.js
+++ game.js
@@ -1,6 +1,26 @@
 const cards = document.querySelectorAll(".card");
-console.log(cards);
+// console.log(cards);
 
-const easyCards = [];
+const card1 = cards[0];
+const card2 = cards[1];
+const card3 = cards[2];
+const card4 = cards[3];
+const card5 = cards[4];
+const card6 = cards[5];
+const card7 = cards[6];
+const card8 = cards[7];
+const card9 = cards[8];
+const card10 = cards[9];
+const card11 = cards[10];
+const card12 = cards[11];
+const card13 = cards[12];
+const card14 = cards[13];
+const card15 = cards[14];
+const card16 = cards[15];
+
+
+const easyCards = ['🐕‍🦺','🐖','🦭','🐍','🐘','🐅','🐐','🐄'];
 const hardCards = [];
 
+// console.log(easyCards[0]);
+card1.innerHTML = easyCards[1];
