4ec3f14dd0698fcbb632941e39788e88c4f3ed64
Sunjang1009 <sunjang1009@gmail.com>
2023-02-26 18:33:04 -0500

    some css update on buttons

--- game.js
+++ game.js
@@ -18,28 +18,51 @@ function clearCards(){
 }
 let player1stTurn=true;
 let player2ndTurn=false;
-
+let playingCards;
 //copy this function .. to shuffle
 function shuffle(array) {
     array.sort(() => Math.random() - 0.5);
 }
 
-//press easy button, then shuffle
+//press easy button, then shuffle, return easycards as playingcards;
+//press hard button, then shuffle, return hardcards as playingcards;
 easyButton.addEventListener('click',()=>{
     //use easyCards for back of the card
     shuffle(easyCards);
+    let playingCards = easyCards;
     console.log('you picked easy cards!');
+    console.log(playingCards)
+    return playingCards;
 });
 
-function findMatchingCards(cards){
-    if(player1stTurn && )
+hardButton.addEventListener('click',()=>{
+    //use easyCards for back of the card
+    shuffle(hardCards);
+    let playingCards = hardCards;
+    console.log('you picked challenge!');
+    console.log(playingCards)
+    return playingCards;
+});
+
+//start game and easy/hard button disable!
+
+function startGame(){
+
+}
+
+function findMatchingCards(card){
+    if(player1stTurn && card.innerHTML === ''){
+        card.innerHTML = easyCards[i];
+        player1stTurn = false;
+        player2ndTurn = true;
+        return true;
+    } 
 }
 
 //real game is here!!
 for (let i=0;i<cards.length;i++){
     cards[i].addEventListener('click',()=>{
-        cards[i].innerHTML = easyCards[i];
-        
+        cards[i].innerHTML = playingCards[i];
         console.log('I have a headache!');
     });
 };
