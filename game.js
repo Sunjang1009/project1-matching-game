83207b2f4265ea4065735448ee5483573c2acb4c
Sunjang1009 <sunjang1009@gmail.com>
2023-02-27 21:09:44 -0500

    update css

--- game.js
+++ game.js
@@ -1,36 +1,52 @@
+//1. all variables//
+//1.1 DOM var
 const cards = document.querySelectorAll(".card");
-// console.log(cards);
-//16 card images
+console.log(cards);
 
 const easyButton = document.querySelector(".easy");
 const hardButton = document.querySelector(".hard");
+// "flipped" will count flipped cards should be 2
+const flippedCards = document.querySelectorAll(".flipped");
+//class : matched
+//class : unmatched
 
-
+//1.2 game related var
 const easyCards = ['🐕‍🦺','🐖','🦭','🐍','🐘','🐅','🐐','🐄','🐕‍🦺','🐖','🦭','🐍','🐘','🐅','🐐','🐄'];
 const hardCards = ['☻','☁︎','❤︎','🐾','❥','☘','⛈','⛇','☻','☁︎','❤︎','🐾','❥','☘','⛈','⛇'];
 let playingCards;
-// playingCards = shuffle(easyCards);
-// console.log(playingCards);
 
-const flippedCards = document.querySelectorAll(".flipped");
+let player1stTurn=true;
+let player2ndTurn=false;
+let playerScore = 0;
+let flippedCardsCount = 0;// for counting for comparison
 
-// console.log(flippedCards);
-
-// // console.log(easyCards[0]);
 
 function clearCards(){
     for (let i=0;i<cards.length;i++){
         cards[i].innerHTML = "";
     };
 }
-let player1stTurn=true;
-let player2ndTurn=false;
+
 
 //copy this function .. to shuffle
 function shuffle(array) {
     shuffleArray = array.sort(() => Math.random() - 0.5);
     return shuffleArray;
 }
+
+function findMatchingCards(card){
+    if(flippedCardsCount<=2){
+        if(player1stTurn && card.innerHTML === ""){
+            card.innerHTML = playingCards[i];
+            flippedCardsCount ++;
+        }
+    }
+    if(flippedCardsCount==2){
+        //how can I compare????
+    }
+
+}
+
 
 //press easy button, then shuffle, return easycards as playingcards;
 //press hard button, then shuffle, return hardcards as playingcards;
@@ -40,7 +56,13 @@ easyButton.addEventListener('click',()=>{
     // console.log(easyCards);//works
     easyButton.classList.add("disabled");
     easyButton.disabled = true;
-
+    //add cards addEventListner
+    for (let i=0;i<cards.length;i++){
+        cards[i].addEventListener('click',()=>{
+            //matching function wants to add//
+            console.log('working!');
+        });
+    };
 });
 
 
@@ -56,55 +78,10 @@ hardButton.addEventListener('click',()=>{
 
 
 
-
-
-
-
-//choose options easy or hard
-
-function chooseEasyHardGame(){
-    if(easyButton.addEventListener('click',()=>{
-        //use easyCards for back of the card
-        playingCards = shuffle(easyCards);
-        console.log('you picked easy cards!');
-        // console.log(playingCards)
-    })){
-        return playingCards;
-    }
-    if(hardButton.addEventListener('click',()=>{
-        //use easyCards for back of the card
-        playingCards = shuffle(hardCards);
-        console.log('you picked challenge!');
-        // console.log(playingCards)
-    })){
-        return playingCards;
-    }
-};
-
-
-//if innerHTML not null 2 cards were flipped, then matching 
-function findMatchingCards(card){
-    for (let i=0;i<playingCards.length;i++){
-        card.innerHTML = playingCards[i];
-    }
-}
-
-
-
-//real game is here!!
-
-
-
-
-// console.log(playingCards);
-
-// chooseEasyHardGame();
-// console.log(playingCards);
-
-for (let i=0;i<cards.length;i++){
-    cards[i].addEventListener('click',()=>{
+// for (let i=0;i<cards.length;i++){
+//     cards[i].addEventListener('click',()=>{
         
-        console.log('I have a headache!');
-    });
-};
+//         console.log('I have a headache!');
+//     });
+// };
 
